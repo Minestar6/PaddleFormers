127f6dbd84c95dd3cfd0553040b40df35f22e3c0
sunny <3197492890@qq.com>
2026-07-21 17:19:01 +0800

    third

--- tests/transformers/seed_oss/test_modeling.py
+++ tests/transformers/seed_oss/test_modeling.py
@@ -20,7 +20,6 @@ import numpy as np
 import paddle
 
 from paddleformers.transformers import (
-    AutoModelForCausalLM,
     SeedOssConfig,
     SeedOssForCausalLM,
     SeedOssForQuestionAnswering,
